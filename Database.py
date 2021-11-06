ba5960a3dd6e9badf50392466d37ca67abda2dc2
MZO9400 <hamza.hameed00@yahoo.com>
2021-11-06 20:16:16 +0500

    Update search API to be one-liner

--- Database.py
+++ Database.py
@@ -9,8 +9,4 @@ class Database:
         del self.data[key]
 
     def search(self, search_key):
-
-        if search_key in self.data:
-            return self.data[search_key]
-        else:
-            return None
+        return self.data[search_key] if search_key in self.data else None
