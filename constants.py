b52af5832686a0cad23f707c080d75c8ffd625d1
MZO9400 <hamza.hameed00@yahoo.com>
2021-11-06 20:30:38 +0500

    Create functions to find distance between nodes

--- constants.py
+++ constants.py
@@ -1,3 +1,4 @@
+import hashlib
 import socket
 
 m = 6  # 2^6 = 64
@@ -10,3 +11,28 @@ def send_message(ip, port, message):
     data = s.recv(1024)
     s.close()
     return data.decode('utf-8')
+
+
+def create_hash(message):
+    digest = hashlib.sha256(message.encode()).hexdigest()
+    digest = int(digest, 16) % pow(2, m)
+    return digest
+
+
+def get_ip_port(string_format):
+    ip, port = str(string_format).split(":")
+    print(ip, port)
+    return ip, int(port)
+
+
+def get_backward_distance_between_nodes(node2, node1):
+    if node2 > node1:
+        return node2 - node1
+    elif node2 < node1:
+        return pow(2, m) - abs(node2 - node1)
+    else:
+        return 0
+
+
+def get_forward_distance_between_nodes(node2, node1):
+    return pow(2, m) - get_backward_distance_between_nodes(node2, node1)
