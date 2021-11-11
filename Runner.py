97f7bb9e9b42b211e1f1e256825ee92a60f37b93
MZO9400 <hamza.hameed00@yahoo.com>
2021-11-11 17:36:51 +0500

    Add print statements to test

--- Runner.py
+++ Runner.py
@@ -17,13 +17,21 @@ class Tests(unittest.TestCase):
             thread.daemon = True
             thread.start()
         Client.client("127.0.0.1", '3000', 'insert', ['1', '2'])
+        print("Insert KV { 1: 2 } on Client 1 @ port 3000")
         Client.client("127.0.0.1", '3010', 'insert', ['2', '3'])
+        print("Insert KV { 2: 3 } on Client 2 @ port 3010")
         Client.client("127.0.0.1", '3020', 'insert', ['3', '4'])
+        print("Insert KV { 3: 4 } on Client 3 @ port 3020")
         Client.client("127.0.0.1", '3030', 'insert', ['4', '5'])
+        print("Insert KV { 4: 5 } on Client 4 @ port 3030")
         Client.client("127.0.0.1", '3000', 'insert', ['5', '6'])
+        print("Insert KV { 5: 6 } on Client 1 @ port 3000")
         self.assertEqual('3', Client.client("127.0.0.1", '3000', 'search', ['2']))
+        print("Test for Key 2 = value 3 on Client 1 @ port 3000")
         self.assertEqual('4', Client.client("127.0.0.1", '3000', 'search', ['3']))
+        print("Test for Key 3 = value 4 on Client 1 @ port 3000")
 
 
 if __name__ == '__main__':
     unittest.main()
+
