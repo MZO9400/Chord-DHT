87ee43edd693c306a2c6008e9751cd3e0fef571b
MZO9400 <hamza.hameed00@yahoo.com>
2021-11-11 16:14:30 +0500

    Set threads to daemon

--- Runner.py
+++ Runner.py
@@ -7,14 +7,15 @@ import spawner
 
 class Tests(unittest.TestCase):
     def test(self):
-        t1 = threading.Thread(target=lambda: spawner.create(3000))
-        t1.start()
-        t2 = threading.Thread(target=lambda: spawner.join(3010, 3000))
-        t2.start()
-        t3 = threading.Thread(target=lambda: spawner.join(3020, 3000))
-        t3.start()
-        t4 = threading.Thread(target=lambda: spawner.join(3030, 3000))
-        t4.start()
+        threads = [
+            threading.Thread(target=lambda: spawner.create(3000)),
+            threading.Thread(target=lambda: spawner.join(3010, 3000)),
+            threading.Thread(target=lambda: spawner.join(3020, 3000)),
+            threading.Thread(target=lambda: spawner.join(3030, 3000))
+        ]
+        for thread in threads:
+            thread.daemon = True
+            thread.start()
         Client.client("127.0.0.1", '3000', 'insert', ['1', '2'])
         Client.client("127.0.0.1", '3010', 'insert', ['2', '3'])
         Client.client("127.0.0.1", '3020', 'insert', ['3', '4'])
